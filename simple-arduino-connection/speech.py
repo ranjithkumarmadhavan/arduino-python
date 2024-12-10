2e88338d3e96eafebdd6fd3db6eabab3000a4697
Ranjith Kumar <43580256+ranjithkumarmadhavan@users.noreply.github.com>
2024-12-10 22:56:59 +0530

    arduino connect with python. also added speech.py

---
+++ simple-arduino-connection/speech.py
@@ -0,0 +1,41 @@
+# Python program to translate
+# speech to text and text to speech
+
+import speech_recognition as sr
+import pyttsx3 
+
+# Initialize the recognizer
+r = sr.Recognizer()
+
+# Function to convert text to speech
+def SpeakText(command):
+    # Initialize the engine
+    engine = pyttsx3.init()
+    engine.say(command)
+    engine.runAndWait()
+
+# Loop infinitely for user to speak
+while True:
+    # Exception handling to handle exceptions at the runtime
+    try:
+        # Use the microphone as source for input
+        with sr.Microphone() as source2:
+            # Wait for a second to let the recognizer adjust the energy threshold
+            # based on the surrounding noise level
+            r.adjust_for_ambient_noise(source2, duration=0.2)
+            
+            # Listen for the user's input
+            audio2 = r.listen(source2)
+            
+            # Using Google to recognize audio
+            MyText = r.recognize_google(audio2)
+            MyText = MyText.lower()
+
+            print("Did you say:", MyText)
+            SpeakText(MyText)
+            
+    except sr.RequestError as e:
+        print(f"Could not request results; {e}")
+        
+    except sr.UnknownValueError:
+        print("Unknown error occurred")
